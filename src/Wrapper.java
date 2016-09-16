d1c60006fd02575c75a543721d514effdc5f6786
Ethan Joseph <ethanrj91@gmail.com>
2016-09-16 08:57:04 -0400

    started to resolved bugs, added git ignore

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -11,43 +11,48 @@ public class Wrapper {
 		
 		while( counter < string.length()) {
 			
-			int marker = 0;
+			int lineMarker = 0;
 			String line = "";
 
-			while (marker <= col) {
+			while (lineMarker <= col) {	
 				
-//				if(remainder.length() <= col){
-//					line = remainder;
-//				}
-//				
-				
-				int index = remainder.indexOf(" "); // index throws -1 when there isn't a space?
+				int spaceIndex = remainder.indexOf(" ");
 				// index is accurately counting number of letters
 				
-				String word = remainder.substring(0, index + 1); //should check to see if the word is shorter or longer than the col number here
+				String word = remainder.substring(0, spaceIndex + 1); 
 				// need the plus one to include the space in the substring
 				
-				if(index == -1){  
+//				if(word.length()>col){
+//					
+//					String wordA = word.substring(0, col);
+//					line = line + "\n"+ wordA;
+//					System.out.println(line);
+//					remainder = remainder.substring(col);
+//					lineMarker = col+1;
+//					
+//				}
+				
+				if(spaceIndex == -1){  
+					
 					if((line + remainder).length()<=col){
 					line = line+remainder;
-					marker = col + 1;
-					break; //not sure if we need this break here... might be able to get rid of them
+					lineMarker = col + 1;
+					//break; 
 					}else if((line + remainder).length()> col){
 					line = line+"\n"+remainder;
-					marker = col+1;
-					break;
+					lineMarker = col+1;
+					
+					//break;
 					}
 				}
 				
-				marker = marker + (index + 1);
+				lineMarker = lineMarker + (spaceIndex + 1);
 				// need to include the spaces after each word
 
-				// marker is accurately tracking number of columns
-
-				if (marker <= (col) || marker - 1 == col) { 
+				if (lineMarker <= (col) || lineMarker - 1 == col) { 
 					line = line.concat(word);
 					
-					remainder = remainder.substring(index + 1);
+					remainder = remainder.substring(spaceIndex + 1);
 					
 				}
 			}
@@ -55,12 +60,13 @@ public class Wrapper {
 			counter = line.length() + counter;
 
 			result = result + line.concat("\n");
+			
 		}
 		return result;
 	}
 
 	public static void main(String[] args) {
-		String test = "i like cheese and crackers";
+		String test = "Hi, my name is bob what is your name?";
 		int col = 5; //bug when you use a col number that is shorter than the longest word and that word is not the last word of the string
 
 		String result = wrapper(test, col);
